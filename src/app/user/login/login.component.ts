7e354506301084d9a58d33f08105765f15e38426
Joshua-Gaspar <joshuagaspar03@gmail.com>
2022-02-24 14:13:44 -0400

    Create login form with reactive form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,30 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  password = new  FormControl('', [
+    Validators.required,
+  ]);
+
+  showAlert = false;
+  alertMsg ='Signing in.';
+  alertColor ='blue';
+
+  loginForm = new FormGroup({
+    email: this.email,
+    password: this.password
+  });
+
+  login(){
+    this.showAlert = true;
+    this.alertMsg ='Signing in.';
+    this.alertColor='blue';
   }
 
 }
