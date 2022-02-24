81e2fd0b835abe562b3b8d570a2b1743243e3fd7
Joshua-Gaspar <joshuagaspar03@gmail.com>
2022-02-24 06:27:39 -0400

    Create register form with reactive form and create and alert component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@ module.exports = {
   purge: {
     content:[
       './src/**/*.{html,ts}'
-    ]
+    ],
+    option: {
+      safelist:['bg-blue-400' , 'bg-green-400', 'bg-red-400']
+    }
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity:['disabled'],
+      backgroundColor:['disabled']
+    },
   },
   plugins: [],
 }
